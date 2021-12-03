07eb2e0ec2a42128f621c55bf511a981ac7a492f
Ricardo Colombo <ricardo.colombo@imaginebeyond.com.br>
2021-12-02 21:53:01 -0400

    init

---
+++ contents/content.js
@@ -0,0 +1,36 @@
+export const content = {
+  name: 'My Dream Team',
+  heading_image: 'heading_mdt.jpg',
+  logo: 'logo.svg',
+  links: ['collection', 'roadmap', 'whitelist'],
+  discord: '#',
+  twitter: '#',
+  welcome:
+    'Metafolks is a collection of 10,000 unique and programmatically generated NFTs from over 600 possible traits, unique digital collectibles living on the Polygon blockchain, making each of them very special. They are all unique, but some are rarer than others.<br /><br />The Metafolks are stores as ERC-721 tokens on the Polygon blockchain and hosted on IPFS. Once released, each Metanoid will cost 8 MATIC.<br /><br />If you want to be one of the first luckiest holders, read the <a href="#whitelist">whitelist</a> section to subscribe.',
+  roadmap: [
+    '<strong>• 12/15/2021</strong> - Whitelist subscription act.',
+    '<strong>• 12/29/2021</strong> - Presale for whitelisted wallets.',
+    '<strong>• 12/31/2021</strong> - Official launch. Open for mint',
+    '<strong>• 12/25/2021</strong> - Xmas giveaway.',
+    '<strong>• 20% minted</strong> - Giveaway act #2.',
+    '<strong>• 40% minted</strong> - Giveaway act #3.',
+    '<strong>• 60% minted</strong> - Giveaway act #4.',
+    "<strong>• 100% minted</strong> - What's next?",
+  ],
+  whitelist:
+    'In december 15 we are going to start the whitelist subscription act. To participate, follow our twitter and discord and keep in touch:',
+  mint: {
+    current_mint_price: '5 MATIC',
+    link: 'https://polygonscan.com/address/0x58596479e520aab7894ed3f44e51c313a8e5caf4#writeContract',
+    videoLink: 'https://www.youtube.com/watch?v=EzWbo5sGMH8',
+    text: 'There are many NFTs read for minting. You can mint yours directly from the link above or bellow on polygonscan. If you don’t know how to mint directly from the smart contract, we recommend the video bellow.',
+  },
+  opensea: {
+    link: 'https://opensea.io/collection/rdsign-my-dream-team',
+    contractAdress:
+      'https://polygonscan.com/address/0x58596479e520aab7894ed3f44e51c313a8e5caf4',
+    colectionName: 'rdsign-my-dream-team',
+  },
+  contract:
+    'https://polygonscan.com/address/0x58596479e520aab7894ed3f44e51c313a8e5caf4#code',
+}
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="layout" :style="`background-image: url(bg.jpg)`">
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+  background-size: 100%;
+  background-repeat: repeat-y;
+  padding: 10vw 0;
+}
+</style>
